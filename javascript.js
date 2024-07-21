250cae2c44f3b7aa0d3e95417f52e676139e5558
Yuvarani25 <145268723+Yuvarani25@users.noreply.github.com>
2024-07-21 14:33:24 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,20 @@
-function add(){
-    var message=document.getElementById("add");
-    message.innerText="add an element";
+function add()
+{
+    var add=document.getElementById("getinput")
+    var input=document.getElementById("input")
+        var litem=document.createElement("li")
+        litem.innerHTML=input.value+" "+"<button onclick='del(event)'>Delete</button>"
+        if(input.value==null||input.value=="")
+        {
+            alert("Enter some text");
+        }
+        else
+        {
+        add.append(litem);
+        }
 }
-function remove(){
-    let dele=document.getElementsByTagName("input")[1];
-    let btn=document.getElementById("del");
-    dele.remove();
-    btn.remove();
+
+function del(event)
+{
+    event.target.parentElement.remove();
 }
